a8543364e047cde945b700ce898dfadcdd97b04e
hamdanesalem <hamdanesalem@gmal.com>
2026-05-27 22:06:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
+class StoreApp(App):
+    def build(self):
+        return Label(text="StoreManager v4.0")
+
+if __name__ == "__main__":
+    StoreApp().run()
